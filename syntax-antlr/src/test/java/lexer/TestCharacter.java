35783dee1c2788ea54762be2c0c96ca3e9fa333d
Keerthikan@Asus <kk.devpro11@gmail.com>
2018-11-06 14:47:29 +0100

    define Character Token and unicode code points

---
+++ syntax-antlr/src/test/java/lexer/TestCharacter.java
@@ -0,0 +1,23 @@
+package lexer;
+
+import bazolang.BazoLangLexer;
+import org.antlr.v4.runtime.Token;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class TestCharacter {
+    @Test
+    public void TestChar() throws IOException {
+        var tokens = LexerUtil.getTokensFromFile("lexer/characters.bl");
+        LexerUtil.assertTotalTokens(tokens, 16);
+
+        var lastToken = tokens.remove(tokens.size() - 1);
+
+        for (int i = 0; i < tokens.size(); i += 2) {
+            Assert.assertEquals(BazoLangLexer.CHARACTER, tokens.get(i).getType());
+        }
+        Assert.assertEquals(Token.EOF, lastToken.getType());
+    }
+}
