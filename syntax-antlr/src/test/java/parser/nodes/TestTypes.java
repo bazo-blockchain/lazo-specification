ccc9a745a3c37715036cefa0ca71a7b147ea1210
Remo Pfister <remo.pfister@hsr.ch>
2018-11-14 09:08:25 +0100

    Add Type tests

--- syntax-antlr/src/test/java/parser/nodes/TestTypes.java
+++ syntax-antlr/src/test/java/parser/nodes/TestTypes.java
@@ -1,5 +1,6 @@
 package parser.nodes;
 
+import bazolang.LazoParser;
 import org.junit.Test;
 import parser.NodeUtil;
 import parser.ParserUtil;
@@ -7,10 +8,63 @@ import parser.ParserUtil;
 public class TestTypes {
     @Test
     public void testInteger() {
-        var parser = ParserUtil.getParserForInput("int x = 5\n");
-        NodeUtil.assertVariableDecl(parser.variableDeclaration(), "x", "int", "5");
-        ParserUtil.assertNoErrors(parser);
+        NodeUtil.assertVariableDecl(getType("int x = 5\n"), "x", "int", "5");
     }
+
+    @Test
+    public void TestBoolean() {
+        NodeUtil.assertVariableDecl(getType("bool x = true\n"), "x", "bool", "true");
+    }
+
+    @Test
+    public void testCharacter() {
+        NodeUtil.assertVariableDecl(getType("char x = 'c'\n"), "x", "char", "'c'");
+    }
+
+    @Test
+    public void testString() {
+        NodeUtil.assertVariableDecl(getType("String x = \"hello\"\n"), "x", "String", "\"hello\"");
+    }
+
+    @Test
+    public void testEmptyStruct() {
+        NodeUtil.assertStructDecl(getStruct("struct X {}\n"), "X", 0);
+    }
+
+    @Test
+    public void testStructOneField() {
+        NodeUtil.assertStructDecl(getStruct("struct X { int a\n }\n"), "X", 1);
+    }
+
+    @Test
+    public void testStructMultipleFields() {
+        NodeUtil.assertStructDecl(getStruct("struct X { int a\n bool b\n }\n"), "X", 2);
+    }
+
+    @Test
+    public void testEnumSingle() {
+        NodeUtil.assertEnumDecl(getEnum("enum X { TEST }\n"), "X", "TEST");
+    }
+
+    @Test
+    public void testEnumMultiple() {
+        NodeUtil.assertEnumDecl(getEnum("enum X { TEST1, TEST2 }\n"), "X", "TEST1", "TEST2");
+    }
+
+//    @Test
+//    public void testMap() {
+//        NodeUtil.assertVariableDecl(getType("int x = 5\n"), "x", "int", "5");
+//    }
+//
+//    @Test
+//    public void testArray() {
+//        NodeUtil.assertVariableDecl(getType("int x = 5\n"), "x", "int", "5");
+//    }
+//
+//    @Test
+//    public void testInterface() {
+//        NodeUtil.assertVariableDecl(getType("int x = 5\n"), "x", "int", "5");
+//    }
 
     // todo test other types from document
 
@@ -18,4 +72,21 @@ public class TestTypes {
 
     // read only variables
 
+    private LazoParser.VariableDeclarationContext getType(String input) {
+        var parser = ParserUtil.getParserForInput(input);
+        ParserUtil.assertNoErrors(parser);
+        return parser.variableDeclaration();
+    }
+
+    private LazoParser.StructDeclarationContext getStruct(String input) {
+        var parser = ParserUtil.getParserForInput(input);
+        ParserUtil.assertNoErrors(parser);
+        return parser.structDeclaration();
+    }
+
+    private LazoParser.EnumDeclarationContext getEnum(String input) {
+        var parser = ParserUtil.getParserForInput(input);
+        ParserUtil.assertNoErrors(parser);
+        return parser.enumDeclaration();
+    }
 }
